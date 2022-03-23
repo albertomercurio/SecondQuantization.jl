eaeb3ef2930763624b32510ac2b3d933053ae5fe
albertomercurio <alberto.mercurio96@gmail.com>
2022-03-23 02:45:33 +0100

    Fixed test problems

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,15 @@
 using SecondQuantization
 using Test
 
+using SymbolicUtils: flatten_term
+
 @testset "SecondQuantization.jl" begin
     # Write your tests here.
 end
 
 @testset "Simplify" begin
     @boson a b c
-    @test isequal(simplify(b * a, threaded_simplifier(100)), a*b)
-    @test isequal(simplify(b * a * b, threaded_simplifier(100)), a*b^2)
-    @test isequal(simplify(b * a * c * b * 2, threaded_simplifier(100)), flatten_term(*, flatten_term(*, 2*a*b^2*c)))
+    @test isequal(simplify(b * a, rewriter = threaded_simplifier(100)), a*b)
+    @test isequal(simplify(b * a * b, rewriter = threaded_simplifier(100)), a*b^2)
+    @test isequal(simplify(b * a * c * b * 2, rewriter = threaded_simplifier(100)), flatten_term(*, flatten_term(*, 2*a*b^2*c)))
 end
